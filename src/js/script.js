dd3601d31d7d2cb737b1f0c478021a12bcaa381f
Ilia Filimonov <9991571155i@gmail.com>
2023-12-01 16:58:32 +0700

    Добавит функционал по добавлению карточек в сайдбар. WIP

--- src/js/script.js
+++ src/js/script.js
@@ -4,23 +4,40 @@ import { Sidebar } from './components/sidebar'
 // второй - кнопка по клику на которую открывается сайдбар,
 // третий положение сайдбара (если не передан, то по умолчанию 'left'
 const panel = new Sidebar('#sidebar', '#open-basket', 'right')
-console.log('panel', panel)
+const form = document.querySelector('#productForm')
+const productList = document.querySelector('.products-list') // контейнер для отрисовки товаров на главной странице
 
-const productList = document.querySelector('.products-list') // контейнер для отрисовки товаров
+// Функция для инициализации основных функций и т.д.
+const init = async () => {
+  window.addEventListener('DOMContentLoaded', async () => {
+    await loadJSON() // функция загрузки данных
 
-loadJSON()
+    // Обработка данных формы
+    form.addEventListener('submit', (e) => {
+      e.preventDefault() // иначе форма отправится синхронно. Это приведет к перезапуску страницы, а значит, метод addProduct не успеет выполниться.
+      addProduct()
+    })
+    await purchaseProduct()
+    // productList.addEventListener('click', purchaseProduct) // добавление товара в корзину ?
+  })
+}
 
+init()
+
+// Функция подгрузки данных на главную страницу
 async function loadJSON() {
+  let html = ''
+
   try {
     const response = await fetch('http://localhost:3000/products')
     const data = await response.json()
 
-    let html = ''
+    console.log('data', data)
 
     if (data && Array.isArray(data)) {
       data.forEach((product) => {
         html += `
-            <div class="main-card">
+            <div class="main-card" data-card-id=${product?.id}>
               <div class="card-image">
                 <img src="${product?.imgSrc}" alt="image">
 
@@ -61,4 +78,64 @@ async function loadJSON() {
   } catch (error) {
     console.error('Ошибка загрузки данных:', error)
   }
+}
+
+// Функция создания товара через форму
+const addProduct = async () => {
+  const productData = {} // отдельная сущность товара
+
+  // собираем данные из формы
+  Array.from(form?.elements).forEach((element) => {
+    if (element?.name) {
+      productData[element?.name] = element?.value
+    }
+  })
+
+  try {
+    const response = await fetch(' http://localhost:3000/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(productData),
+    })
+
+    if (response.ok) {
+      console.log('Product added successfully!')
+      form.reset()
+      loadJSON() // показываем актуальные данные
+    } else {
+      console.log('Error to add product.')
+    }
+  } catch (error) {
+    console.error('Error', error)
+  }
+}
+
+// Функция добавления товара в корзину
+function purchaseProduct() {
+  const cards = document.querySelectorAll('.main-card') // получаем все карточки
+
+  cards.forEach((card) => {
+    card.addEventListener('click', () => getProductInfo(card))
+  })
+}
+
+// Функция извлечения данных из карточки на главной странице
+function getProductInfo(product) {
+  const productInfo = {
+    id: product?.querySelector('.main-card')?.dataset?.сardId ?? '',
+    imgSrc: product?.querySelector('.card-image img')?.src ?? '',
+    name: product?.querySelector('.card-name')?.textContent ?? '',
+    category: product?.querySelector('.card-category')?.textContent ?? '',
+    price: product?.querySelector('.card-price')?.textContent ?? '',
+  }
+
+  addProductsToBasketList(productInfo)
+}
+
+function addProductsToBasketList(basketItem) {
+  console.log(basketItem)
+
+  // рисуем данные с сайдбаре
 }
